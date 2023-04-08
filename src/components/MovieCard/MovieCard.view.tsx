942ed5ec1ed52de935c6f2400944cb3df3cec710
metalboyrick <rsulisthio@gmail.com>
2023-04-08 22:52:43 +0700

    feat: responsive design for movie card

--- src/components/MovieCard/MovieCard.view.tsx
+++ src/components/MovieCard/MovieCard.view.tsx
@@ -26,22 +26,32 @@ function MovieCard({
       >
         <Flex
           flexDirection={{ md: "column" }}
-          alignItems={"flex-start"}
+          width={["100%", "100%", "150px"]}
+          alignItems={["center", "center", "flex-start"]}
           bgColor={"white"}
-          maxWidth="200px"
           _hover={{
             bgColor: "teal.50",
           }}
           cursor={"pointer"}
         >
-          <Image src={img} alt={"Movie Poster"} width={200} height={300} />
-          <VStack alignItems={"left"} padding={2} width="100%">
-            <Text fontWeight={"bold"} color="teal">
-              {title}
-            </Text>
-            <Text color="gray" fontSize="sm">
-              {starring}
-            </Text>
+          <Image src={img} alt={"Movie Poster"} width={150} height={225} />
+          <Flex
+            alignItems={"left"}
+            padding={2}
+            width="100%"
+            height={["225px", "225px", "150px", "150px"]}
+            justifyContent={"space-between"}
+            direction={"column"}
+          >
+            <VStack align={"left"}>
+              <Text fontWeight={"bold"} color="teal">
+                {title}
+              </Text>
+              <Text color="gray" fontSize="sm">
+                {starring}
+              </Text>
+            </VStack>
+
             <Flex
               justifyContent={"space-between"}
               fontWeight={"bold"}
@@ -58,7 +68,7 @@ function MovieCard({
                 </Text>
               </HStack>
             </Flex>
-          </VStack>
+          </Flex>
         </Flex>
       </Link>
     </>
